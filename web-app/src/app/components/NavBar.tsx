36001637aec893a7584fc09d6c82e207b47bc23e
Toppuccino <162898297+Toppuccino@users.noreply.github.com>
2024-03-11 15:14:55 +0700

    Create NavBar

---
+++ web-app/src/app/components/NavBar.tsx
@@ -0,0 +1,32 @@
+// components/NavBar.tsx
+
+export const NavBar = () => {
+    return (
+    
+        <div className="navBar">
+          <div className="navContainer">
+  
+            <div className="logo">
+              Infiltrix
+            </div>
+  
+            <nav>
+              <ul className="md:flex md:items-center">
+                <li className="navLi"><a href="#">Home</a></li>
+                <li className="navLi"><a href="#">About</a></li>
+                <li className="navLi"><a href="#">Contact</a></li>
+              </ul>
+            </nav>
+  
+            <div className="pr-20">
+              <div className="btn">
+                Contact Us
+              </div>
+            </div>
+  
+          </div>
+        </div>
+   
+    );
+  };
+  
