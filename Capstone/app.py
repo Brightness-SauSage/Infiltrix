9c10d69e450c61fd8b23d6b3af99f27a71864129
Sawatsakron <pithwithsomeonelol@gmail.com>
2024-03-12 20:37:43 +0700

    Update Model and API 2
    .

---
+++ Capstone/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+with open('Capstone\model.pkl', 'rb') as f:
+    detection = pickle.load(f)
+
+@app.route('/predict', methods=['POST'])
+
+
+def predict():
+
+    data = request.get_json()
+    input_str = data['TEXT']
+    
+    result = detection(input_str)
+    
+    return jsonify({'prediction ': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
