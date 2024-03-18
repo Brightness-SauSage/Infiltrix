c86aa0cce7e29a69093f68391e227b9c0ab622b5
Toppuccino <162898297+Toppuccino@users.noreply.github.com>
2024-03-19 02:59:13 +0700

    update

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -1,45 +1,59 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import { useState } from 'react';
+import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
-  
   return (
-
     <div className="navbar rounded-box bg-gray-100 glass">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-96"
+          >
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+
+            <li>
+              <Link href="/train">Train our model</Link>
+            </li>
+          </ul>
+        </div>
+        <a className="logo text-xl navbar-center">Infiltrix</a>
       </div>
-      <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-96">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        
-        <li>
-        <Link href="/train">Train our model</Link>
-        </li>
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+
+          <li>
+            <Link href="/train">Train our model</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-neutral hidden lg:block">Contact us</button>
+      </div>
     </div>
-    <a className="logo text-xl navbar-center">Infiltrix</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    <li>
-          <Link href="/">Home</Link>
-        </li>
-        
-        <li>
-        <Link href="/train">Train our model</Link>
-        </li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <button className="btn btn-neutral hidden md:block">Contact us</button>
-  </div>
-  </div>
 
     /*
     <div className="p-4 bg-gray-50 shadow">
@@ -71,6 +85,5 @@ export default function NavBar() {
       </div>
     </div>
 {*/
-  
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,55 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { KeyboardEvent } from "react";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [showError, setShowError] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [finalResult, setFinalResult] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
-    setShowError(false); // Reset error state when input changes
+    setShowError(false);
   };
 
   const handleSubmit = () => {
-    if (inputValue.trim() === '') {
-      setShowError(true); // Show error if input is empty
+    if (inputValue.trim() === "") {
+      setShowError(true);
     } else {
-      // Handle submission
-      console.log(inputValue);
+      setModalText(inputValue);
+      setFinalResult("Spam");
+      openModal();
+      setIsLoading(true);
+      simulateLoading();
     }
   };
 
   const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSubmit();
     }
+  };
+
+  const openModal = () => {
+    setShowModal(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    document.body.style.overflow = "auto";
+    setModalText("");
+    setInputValue("");
+  };
+
+  const simulateLoading = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   };
 
   return (
@@ -35,10 +60,9 @@ export default function Home() {
             Message Spam Detector
           </div>
           <div className="text-gray-400 text-4xl merriweather font-normal m-2 md:text-5xl lg:text-6xl relative">
-  for everyone
-  <span className="animate-blink">!</span>
-</div>
-
+            for everyone
+            <span className="animate-blink">!</span>
+          </div>
         </div>
         <div>
           <div className="container flex justify-center items-center">
@@ -49,15 +73,15 @@ export default function Home() {
               <input
                 type="text"
                 className={`inter font-light text-base border ${
-                  showError ? 'border-red-500' : 'border-gray-300'
+                  showError ? "border-red-500" : "border-gray-300"
                 } h-12 w-96 pl-6 pr-20 md:h-12 md:w-96 md:pl-6 md:pr-20 rounded-full z-0 focus:shadow focus:outline-none`}
                 placeholder="paste your message here"
                 value={inputValue}
                 onChange={handleInputChange}
-                onKeyPress={handleKeyPress} // Add key press event handler
+                onKeyPress={handleKeyPress}
               />
               <div className="absolute top-2 right-2">
-                <button 
+                <button
                   className="bg-gray-400 rounded-full hover:transition duration-500 hover:bg-gray-500"
                   onClick={handleSubmit}
                 >
@@ -92,20 +116,74 @@ export default function Home() {
           </div>
         )}
 
-    {/*
+        {/*
         <div
           className="mt-8 h-96 w-screen"
           style={{ background: 'linear-gradient(70deg, #a855f7, #ec4899)' }}
         />     
         */}
 
-      
         <div className="mt-10 h-96 w-screen relative overflow-hidden">
-    <img src="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif" 
-    className="absolute top-0 left-0 transform scale-150 saturate-150"/>
-</div>
+          <img
+            src="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif"
+            className="absolute top-0 left-0 transform scale-150 saturate-150"
+          />
+        </div>
 
+        {showModal && (
+          <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+            <div className="relative bg-white p-8 rounded-lg h-48 w-96 overflow-hidden">
+              <button
+                onClick={closeModal}
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-600 cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
 
+              <div className="flex justify-center">
+                <div className="flex flex-col justify-center">
+                  {isLoading ? (
+                    <>
+                      <span className="loading loading-dots loading-lg w-20"></span>
+                      <p className="font-bold text-xl">Analyzing ...</p>
+                    </>
+                  ) : (
+                    <div className="flex flex-col items-center">
+                      <div className="flex flex-col items-center m-2">
+                        <p className="font-bold text-xl">Text Input:</p>
+
+                        <p className="text-xl overflow-auto max-h-16">
+                          {modalText}
+                        </p>
+                      </div>
+
+                      <div className="flex items-center m-3">
+                        <p className="font-bold text-xl">Result:&nbsp;</p>
+
+                        <p className="text-xl overflow-auto max-h-16">
+                          {finalResult}
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
