ccdee04553e45ba62813de1d9837b6cf5f565c2f
Toppuccino <162898297+Toppuccino@users.noreply.github.com>
2024-03-20 02:00:03 +0700

    increase form size

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -106,59 +106,119 @@ export default function Home() {
             <span className="animate-blink">!</span>
           </div>
         </div>
-        <div>
-          <div className="container flex justify-center items-center">
-            <div className="relative">
-              <div className="absolute top-4 left-3">
-                <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+
+        {/*
+        <div className="flex flex-col justify-center items-center">
+          <div className="relative">
+            <div className="absolute top-4 left-3">
+              <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+            </div>
+            <input
+              type="text"
+              className={`inter font-light text-base border ${
+                showError ? "border-red-500" : "border-gray-300"
+              } h-12 w-96 pl-6 pr-20 md:h-12 md:w-96 md:pl-6 md:pr-20 rounded-full z-0 focus:shadow focus:outline-none`}
+              placeholder="paste your message here"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+            />
+            <div className="absolute top-2 right-2">
+              <div className="tooltip" data-tip="Submit">
+                <button
+                  className="bg-gray-400 rounded-full hover:transition duration-500 hover:bg-gray-500"
+                  onClick={handleSubmit}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="white"
+                    className="bi bi-arrow-right-short"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"
+                    />
+                  </svg>
+                </button>
               </div>
-              <input
-                type="text"
-                className={`inter font-light text-base border ${
+            </div>
+          </div>
+
+          {showError && (
+            <div className="font-thin text-xs text-red-500 m-2 inter">
+              The field can't be empty!
+            </div>
+          )}
+
+          {!showError && (
+            <div className="font-thin text-xs text-gray-400 m-2 inter">
+              Try pasting something!
+            </div>
+          )}
+        </div>
+
+        */}
+
+        <div className="flex flex-col justify-center items-center">
+          <div className="relative">
+            <div className="absolute top-4 left-3">
+              <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+            </div>
+
+            <div className="max-w-2xl mx-auto">
+              <textarea
+                id="message"
+                className={`${
                   showError ? "border-red-500" : "border-gray-300"
-                } h-12 w-96 pl-6 pr-20 md:h-12 md:w-96 md:pl-6 md:pr-20 rounded-full z-0 focus:shadow focus:outline-none`}
+                } pr-14 block p-2.5 transform lg:w-[420px] md:w-[360px] w-[300px] h-[100px] text-md text-gray-700 bg-white rounded-lg border border-gray-300 z-0 focus:shadow focus:outline-none`}
                 placeholder="paste your message here"
                 value={inputValue}
                 onChange={handleInputChange}
                 onKeyPress={handleKeyPress}
-              />
-              <div className="absolute top-2 right-2">
-                <div className="tooltip" data-tip="Submit">
-                  <button
-                    className="bg-gray-400 rounded-full hover:transition duration-500 hover:bg-gray-500"
-                    onClick={handleSubmit}
+              ></textarea>
+
+              <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+            </div>
+
+            <div className="absolute bottom-2 right-3">
+              <div className="tooltip" data-tip="Submit">
+                <button
+                  className="bg-gray-400 rounded-full hover:transition duration-500 hover:bg-gray-500"
+                  onClick={handleSubmit}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="white"
+                    className="bi bi-arrow-right-short"
+                    viewBox="0 0 16 16"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      fill="white"
-                      className="bi bi-arrow-right-short"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"
-                      />
-                    </svg>
-                  </button>
-                </div>
+                    <path
+                      fillRule="evenodd"
+                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"
+                    />
+                  </svg>
+                </button>
               </div>
             </div>
           </div>
+
+          {showError && (
+            <div className="font-thin text-xs text-red-500 m-2 inter">
+              The field can't be empty!
+            </div>
+          )}
+
+          {!showError && (
+            <div className="font-thin text-xs text-gray-400 m-2 inter">
+              Try pasting something!
+            </div>
+          )}
         </div>
-
-        {showError && (
-          <div className="font-thin text-xs text-red-500 m-2 inter">
-            The field can't be empty!
-          </div>
-        )}
-
-        {!showError && (
-          <div className="font-thin text-xs text-gray-400 m-2 inter">
-            Try pasting something!
-          </div>
-        )}
 
         {/*
         <div
