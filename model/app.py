4283f6e9dfce6b81fc85168718304c99ef3c4427
22Frankfert <102750170+22Frankfert@users.noreply.github.com>
2024-05-04 03:12:09 +0700

    feat: implement database

--- model/app.py
+++ model/app.py
@@ -1,56 +1,28 @@
 from flask import Flask, request, jsonify
-import pickle
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer
-import scipy.sparse as sp
 from flask_cors import CORS
+from utils import *
+import supabase
+import database.setting as setting
+from supabase import create_client, Client
 
 app = Flask(__name__)
 CORS(app)
 
+supabase: Client = create_client(setting.url, setting.key)
+
 
 @app.route("/api/predict", methods=["POST"])
 def predict():
-
-    def detect_url(input_str):
-        url_pattern = re.compile(r"https?://\S+|www\.\S+|bit\.ly/\S+")
-        return bool(re.search(url_pattern, input_str))
-
-    def detect_email(input_str):
-        email_pattern = re.compile(
-            r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
-        )
-        return bool(re.search(email_pattern, input_str))
-
-    def detect_phone(input_str):
-        phone_pattern = re.compile(r"\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b")
-        return bool(re.search(phone_pattern, input_str))
-
-    def detection(input_str):
-        model = pickle.load(open("model.pkl", "rb"))
-        vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
-        user_url = detect_url(input_str)
-        user_email = detect_email(input_str)
-        user_phone = detect_phone(input_str)
-
-        input_str_tfidf = vectorizer.transform([input_str])
-
-        input_str_combined = sp.hstack(
-            (input_str_tfidf, [[user_url, user_email, user_phone]]), format="csr"
-        )
-
-        predicted_label = model.predict(input_str_combined)
-
-        if predicted_label == 2:
-            return 0  #'spam'
-        elif predicted_label == 1:
-            return 2  #'smishing'
-        else:
-            return 1  #'not spam'
-
     data = request.get_json(force=True)
     input_str = data["Text"]
     prediction = detection(input_str)
+
+    response = (
+        supabase.table("countries")
+        .insert({"input": input_str, "prediction": prediction})
+        .execute()
+    )
+    print(response)
 
     return jsonify({"prediction": prediction})
 
