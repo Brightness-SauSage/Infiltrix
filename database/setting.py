4b9e46b768cc602c1c8c05162407333ead0b2d9d
22Frankfert <102750170+22Frankfert@users.noreply.github.com>
2024-05-05 21:14:45 +0700

    feat: delete files for deployment

--- database/setting.py
+++ database/setting.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
+url = "https://mvfhvnizyecspjbbfbhd.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12Zmh2bml6eWVjc3BqYmJmYmhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3NTcwMjUsImV4cCI6MjAzMDMzMzAyNX0.pXULUtKaP8ex-BDWhX4kmkenHX9rb3XreEEJqK-O4nI"
